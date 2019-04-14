f0f776393dd49ddb322ee52fb46bef0a14439c66
Natalia Franquet <n.franquet.lm@gmail.com>
2019-04-14 18:57:22 +0200

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,37 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require("../models/Movie");
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movie
+    .find({})
+    .then((movies) => {
+      // console.log(movies)
+      res.render('movies', { movies });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get('/movie/:_id', (req, res, next) => {
+  var movieId = req.params._id
+  Movie
+    .findById({ "_id": movieId })
+    .then((movies) => {
+      // console.log(movie)
+      res.render("movie", { movies })
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 
 module.exports = router;
